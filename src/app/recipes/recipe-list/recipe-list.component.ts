cd52026b8223390ed9aaf9d474887fd8a435144d
Puchoo <fede.madoery@hotmail.com>
2017-09-21 17:38:22 -0300

    Recipe Detail Add Shopping List Add Shopping List Edit Add *All in one Page -> Change to separated pages

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('Receta de prueba','Esta es una receta de prueba - No es rica','http://www.essen.com.ar/imgs/w300-h400-c300.400/contenido/editor/Image/2015/recetas-essen-por-tipo-de-comida.jpg')
+
+  ];
 
   constructor() { }
 
